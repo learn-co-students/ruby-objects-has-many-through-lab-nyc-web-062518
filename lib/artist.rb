8510ec475ea309d25e687603793be8f6da7cb33c
Tony Turetsky <TTuretsky@gmail.com>
2018-07-02 16:48:28 -0400

    Done.

---
+++ lib/artist.rb
@@ -0,0 +1,38 @@
+require 'pry'
+
+class Artist
+
+
+attr_accessor :name, :genre
+
+@@all = []
+
+  def initialize(name)
+    @name = name
+    @songs = []
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def songs
+    Song.all.select do |song|
+      song.artist == self
+    end
+  end
+
+  def new_song(name, genre)
+    Song.new(name, self, genre)
+  end
+
+  def genres
+    songs.map do |song|
+      song.genre
+    end
+  end
+
+
+
+end
